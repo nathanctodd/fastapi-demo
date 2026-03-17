bf5a1d63e74b8ca0375e9110e06422cf795469ae
nathanctodd <nathanctodd@gmail.com>
2026-03-17 16:33:22 -0400

    Updated tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,14 +1,4 @@
 
-    
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-def test_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    
 def test_basic():
     assert 1 + 1 == 2
     
